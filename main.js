9b41585e926330f1a09817b4ea777b8e4b94b53a
muzichkar <muzichkar@gmail.com>
2022-01-13 16:52:07 +0200

    test

--- main.js
+++ main.js
@@ -1,61 +1,95 @@
-function CoffeeMake(model) {
-	this.model = model
+
+// task 1
+
+// class Worker {
+// 	constructor(name, surname, rate, days) {
+// 		this.name = name;
+// 		this.surname = surname;
+// 		this.rate = rate;
+// 		this.days = days;
+// 	}
+// 	getSalary() {
+// 		return worker.days * worker.rate;
+// 	}
+// }
+
+// let worker = new Worker('Ivan', 'Ivanov', '10', '31');
+// console.log(worker.name);
+// console.log(worker.surname);
+// console.log(worker.rate);
+// console.log(worker.days);
+// console.log(worker.getSalary());
+
+// task 2
+
+// class MyString {
+// 	reverse(str) {
+// 		return str.split('').reverse().join('')
+// 	}
+// 	ucFirst(word) {
+// 		let newWord = word[0].toUpperCase();
+// 		for (let i = 1; i < word.length; i++) {
+// 			newWord += word[i].toLowerCase();
+// 		}
+// 		return newWord;
+// 	}
+// 	ucWords(words) {
+// 		let newWord;
+// 		let ucWord = '';
+// 		let t1 = words.split(' ')
+// 		for (let i = 0; i < t1.length; i++) {
+
+// 			let word = t1[i]
+// 			newWord = word[0].toUpperCase();
+// 			for (let i = 1; i < word.length; i++) {
+// 				newWord += word[i].toLowerCase();
+// 			}
+// 			ucWord += newWord + ' '
+// 		}
+// 		return ucWord
+// 	}
+
+// }
+// const str = new MyString();
+// console.log(str.reverse('IVAN'));
+// console.log(str.ucFirst('arsenal'));
+// console.log(str.ucWords('arsenal arsenal arsenal'));
+
+class CoffeeMake {
+	constructor(model) {
+		this.model = model
+	}
+	on() {
+		return `on`
+	}
+	off() {
+		return `off`
+	}
 }
 
-CoffeeMake.prototype.on = function () {
-	console.log(`on`);
-
+class Drip extends CoffeeMake {
+	choice() {
+		return `Зробіть свій вибір`
+	}
 }
-CoffeeMake.prototype.off = function () {
-	console.log(`of`);
-}
-// -------------------------
-
-
-function Drip(model, specification) {
-	CoffeeMake.call(this, model);
-	this.specification = specification;
-}
-
-Drip.prototype = Object.create(CoffeeMake.prototype);
-Drip.prototype.constuctor = Drip;
-Drip.prototype.choice = function () {
-	console.log(`Зробіть свій вибір`)
-}
-
 let drip = new Drip();
-drip.on()
-drip.choice();
+console.log(drip.on());
+console.log(drip.choice());
 
-// -------------------------
-
-function Carob(model, specification) {
-	CoffeeMake.call(this, model);
-	this.specification = specification;
+class Carob extends CoffeeMake {
+	end() {
+		return `Заберіть напій`
+	}
 }
-Carob.prototype = Object.create(CoffeeMake.prototype);
-Carob.prototype.constuctor = Carob;
-Carob.prototype.end = function () {
-	console.log(`Заберіть напій`);
-}
-
 let carob = new Carob();
-carob.on()
-carob.end();
+console.log(carob.on());
+console.log(carob.end());
 
-// -------------------------
-
-function CoffeMachine(model, specification) {
-	CoffeeMake.call(this, model);
-	this.specification = specification;
+class CoffeMachine extends CoffeeMake {
+	call() {
+		return `Додати молока?`
+	}
 }
-
-CoffeMachine.prototype = Object.create(CoffeeMake.prototype)
-CoffeMachine.prototype.constuctor = CoffeMachine;
-CoffeMachine.prototype.milk = function () {
-	console.log(`Додати молока?`);
-}
-
-let mashine = new CoffeMachine();
-mashine.off()
-mashine.milk();
+let coffeMachine = new CoffeMachine();
+console.log(coffeMachine.off());
+console.log(coffeMachine.call());
